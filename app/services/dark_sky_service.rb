27d962244aecc3abd80846b42597f29b56db7486
Scott Thomas <smthom05@gmail.com>
2019-04-17 11:49:14 -0600

    added caching to darksky service and flickr service

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -15,9 +15,11 @@ class DarkSkyService
   private
 
   def conn
-    Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_KEY']}/#{@lat},#{@lng}?exclude=minutely,alerts,flags") do |faraday|
-      faraday.headers['Content-Type'] = 'application/json'
-      faraday.headers['Accept'] = 'application/json'
+    Rails.cache.fetch("#{@lat},#{@lng}/#{Time.now.hour}/weather", expires_in: 1.hour) do
+      Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_KEY']}/#{@lat},#{@lng}?exclude=minutely,alerts,flags") do |faraday|
+        faraday.headers['Content-Type'] = 'application/json'
+        faraday.headers['Accept'] = 'application/json'
+      end
     end
   end
 end
